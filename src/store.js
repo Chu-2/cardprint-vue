c6586780b797779b4ef9804a15d8703d0081e833
Ruiqi Yu <richyu@email.arizona.edu>
2019-04-25 13:00:58 -0700

    Refactor store.js

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
@@ -7,59 +7,53 @@ const initialState = () => ({
   selectedCardIndex: 0,
   cards: [
     {
-      background: '#ffffff',
-      foreground: '#000000',
-      foregroundControl: 'dark',
-      width: '2.75in',
-      height: '2.75in',
-      sizeControl: 'big',
-      number: '',
-      subject: '',
-      tracker: '',
-      product: '',
+      background: "#ffffff",
+      foreground: "#000000",
+      foregroundControl: "dark",
+      width: "2.75in",
+      height: "2.75in",
+      sizeControl: "big",
+      number: "",
+      subject: "",
+      tracker: "",
+      product: ""
     }
   ]
 });
 
-const state = initialState();
-
-const selectedCard = () => state.cards[state.selectedCardIndex];
-
-const getters = {
-  getSelectedCard() {
-    return selectedCard();
-  }
-};
-
-const mutations = {
-  createCard(state, payload) {
-    state.cards.push(payload);
-    state.selectedCardIndex = state.cards.length - 1;
-  },
-  removeSelectedCard(state) {
-    if (state.cards.length === 1) {
-      return;
-    }
-    state.cards.splice(state.selectedCardIndex, 1);
-
-    state.selectedCardIndex = state.selectedCardIndex === state.cards.length ?
-      state.selectedCardIndex - 1 : state.selectedCardIndex;
-  },
-  resetState(state) {
-    const initial = initialState();
-    Object.keys(initial).forEach(key => state[key] = initial[key]);
-  },
-  updateSelectedCardIndex(state, index) {
-    state.selectedCardIndex = index;
-  },
-  updateCard(state, payload) {
-    const newCard = {...state.cards[state.selectedCardIndex], ...payload};
-    state.cards.splice(state.selectedCardIndex, 1, newCard);
-  }
-};
-
 export default new Vuex.Store({
-  state,
-  getters,
-  mutations
+  state: initialState(),
+  getters: {
+    getSelectedCard(state) {
+      return state.cards[state.selectedCardIndex];
+    }
+  },
+  mutations: {
+    createCard(state, payload) {
+      state.cards.push(payload);
+      state.selectedCardIndex = state.cards.length - 1;
+    },
+    removeSelectedCard(state) {
+      if (state.cards.length === 1) {
+        return;
+      }
+      state.cards.splice(state.selectedCardIndex, 1);
+
+      state.selectedCardIndex =
+        state.selectedCardIndex === state.cards.length
+          ? state.selectedCardIndex - 1
+          : state.selectedCardIndex;
+    },
+    resetState(state) {
+      const initial = initialState();
+      Object.keys(initial).forEach(key => (state[key] = initial[key]));
+    },
+    updateSelectedCardIndex(state, index) {
+      state.selectedCardIndex = index;
+    },
+    updateCard(state, payload) {
+      const newCard = { ...state.cards[state.selectedCardIndex], ...payload };
+      state.cards.splice(state.selectedCardIndex, 1, newCard);
+    }
+  }
 });
